98cb7bfe35da668a9a774c3157dc770e8b753b8a
David Golke <golkedj@gmail.com>
2023-08-28 14:57:31 -0500

    Section 8 progress

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,5 +1,5 @@
 import { createStandaloneToast } from '@chakra-ui/react';
-import { QueryCache, QueryClient } from 'react-query';
+import { MutationCache, QueryCache, QueryClient } from 'react-query';
 
 import { theme } from '../theme';
 
@@ -16,9 +16,9 @@ function queryErrorHandler(error: unknown): void {
 // to satisfy typescript until this file has uncommented contents
 export const queryClient = new QueryClient({
   queryCache: new QueryCache({ onError: queryErrorHandler }),
+  mutationCache: new MutationCache({ onError: queryErrorHandler }),
   defaultOptions: {
     queries: {
-      onError: queryErrorHandler,
       staleTime: 600000, // 10 minutes
       cacheTime: 900000, // 15 minutes
       refetchOnMount: false,
